03f49d474b79e240b5a109b3f83ed8da06984c0c
Le127 <nereocarbonara@gmail.com>
2021-10-08 10:50:45 -0300

    splash_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:api_clima/pages/home_page.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,7 +13,15 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       initialRoute: 'home',
-      routes: {'home': (_) => const HomePage()},
+      routes: {
+        'home': (_) => AnimatedSplashScreen(
+            splashIconSize: 200,
+            splash: Icons.home,
+            // backgroundColor: const Color(0xFF0D47A1),
+            splashTransition: SplashTransition.fadeTransition,
+            pageTransitionType: PageTransitionType.fade,
+            nextScreen: const HomePage()),
+      },
     );
   }
 }
