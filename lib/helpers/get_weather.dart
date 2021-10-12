3bebd51d5ff4240cad38b595dd7c0ab1e8f759ab
Le127 <nereocarbonara@gmail.com>
2021-10-12 17:13:32 -0300

    api_forecast

--- lib/helpers/get_weather.dart
+++ lib/helpers/get_weather.dart
@@ -1,3 +1,4 @@
+import 'package:api_clima/class/city_forecast.dart';
 import 'package:http/http.dart' as http;
 import 'package:api_clima/class/city_weather.dart';
 
@@ -13,16 +14,19 @@ Future<CityWeather> getWeather(String city) async {
   }
 }
 
-
-/* Future<CityWeather> getWeather(String city) async {
+Future<CityForecast> getForecast(double lon, double lat) async {
   final url = Uri.parse(
-      "http://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=252a7ec697ac784e8a0e7a6bc28d2aaf");
-  final get = await http.get(url);
-  if (get.statusCode == 200) {
-    final city = cityWeatherFromJson(get.body);
-    return city;
-  } else {
-    throw Exception('Failed to load');
+      "https://api.openweathermap.org/data/2.5/onecall?lat=$lat&lon=$lon&exclude=current,minutely,hourly,alerts&appid=252a7ec697ac784e8a0e7a6bc28d2aaf");
+  /* final url = Uri.parse(
+      "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=current,minutely,hourly,alerts&appid=252a7ec697ac784e8a0e7a6bc28d2aaf"); */
+
+  try {
+    final get = await http.get(url);
+    final cityForecast = cityForecastFromJson(get.body);
+
+    //print(cityForecast.daily!.first.clouds);
+    return cityForecast;
+  } catch (e) {
+    return CityForecast(timezone: 'Pronostico no encontrado');
   }
 }
- */
